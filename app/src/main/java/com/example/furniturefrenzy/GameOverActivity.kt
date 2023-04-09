72c1d0425c646495902ebbe5c3e4211b220abb7f
S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>
2023-04-09 18:25:48 +0800

    feat: pass score to gameover activity
    Signed-off-by: S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>

--- app/src/main/java/com/example/furniturefrenzy/GameOverActivity.kt
+++ app/src/main/java/com/example/furniturefrenzy/GameOverActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class GameOverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
+
+        // Get the score extra from the intent
+        val score = intent.getIntExtra("SCORE", 0)
+        val scoreTextView: TextView = findViewById(R.id.scoreTextView)
+        scoreTextView.text = "Score: $score"
 
         val homeButton = findViewById<Button>(R.id.home)
         homeButton.setOnClickListener{
