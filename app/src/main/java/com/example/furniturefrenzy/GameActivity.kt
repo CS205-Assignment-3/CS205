cca96b87d13107a245e9b357a4ea2ebbbb09934c
Haiyang Xu <haiyang.xu.2021@scis.smu.edu.sg>
2023-04-11 15:46:31 +0800

    Update background + buttons configuration

--- app/src/main/java/com/example/furniturefrenzy/GameActivity.kt
+++ app/src/main/java/com/example/furniturefrenzy/GameActivity.kt
@@ -10,7 +10,18 @@ class GameActivity : AppCompatActivity() {
     private lateinit var game: Game
     private lateinit var scoreTextView: TextView
     private lateinit var workersTextView: TextView
-    private lateinit var craftOrderButton: Button
+    private lateinit var craftCoffeeTableButton: Button
+    private lateinit var craftFoldingChairButton: Button
+    private lateinit var craftGlassTableButton: Button
+    private lateinit var craftParkBenchButton: Button
+    private lateinit var craftPlasticChairButton: Button
+    private lateinit var craftStoneBenchButton: Button
+    private lateinit var craftStoneTable: Button
+    private lateinit var woodStationButton: Button
+    private lateinit var stoneStationButton: Button
+    private lateinit var glassStationButton: Button
+    private lateinit var oreStationButton: Button
+    private lateinit var plasticStationButton: Button
     private lateinit var gameOverButton: Button
     private val workerCount = 5 // Set number of worker here
 
@@ -26,20 +37,71 @@ class GameActivity : AppCompatActivity() {
 
 
         // Action listeners
-        craftOrderButton = findViewById(R.id.craftOrder)
+        craftCoffeeTableButton = findViewById(R.id.coffeeTable)
+        craftFoldingChairButton = findViewById(R.id.foldingChair)
+        craftGlassTableButton = findViewById(R.id.glassTable)
+        craftParkBenchButton = findViewById(R.id.parkBench)
+        craftPlasticChairButton = findViewById(R.id.plasticChair)
+        craftStoneBenchButton = findViewById(R.id.stoneBench)
+        craftStoneTable = findViewById(R.id.stoneTable)
         gameOverButton = findViewById(R.id.gameOver)
 
         // 5x resource sites
-        // rockStationButton... {game.extractResource(1)}
+        // Wood -> 1; Stone -> 2; Glass -> 3; Ore -> 4; Plastic -> 5
+        woodStationButton = findViewById(R.id.forest)
+        woodStationButton.setOnClickListener {
+            game.extractResource(1)
+        }
 
+        stoneStationButton = findViewById(R.id.digsite)
+        stoneStationButton.setOnClickListener {
+            game.extractResource(2)
+        }
 
-        // 6x crafting bench (Show dialogbox)
+        glassStationButton = findViewById(R.id.smelters)
+        glassStationButton.setOnClickListener {
+            game.extractResource(3)
+        }
 
+        oreStationButton = findViewById(R.id.mine)
+        oreStationButton.setOnClickListener {
+            game.extractResource(4)
+        }
 
+        plasticStationButton = findViewById(R.id.factory)
+        plasticStationButton.setOnClickListener {
+            game.extractResource(5)
+        }
+
+        // 6x crafting bench (Inside dialogbox)
         // Produce crafting buttons
-        // Plastic chair craft button
-        craftOrderButton.setOnClickListener {
+        // coffee table -> 1; folding chair -> 2; glass table -> 3; park bench -> 4; plastic chair -> 5; stone bench -> 6; stone table -> 7
+        craftCoffeeTableButton.setOnClickListener {
             game.craftOrder(1)
+        }
+
+        craftFoldingChairButton.setOnClickListener {
+            game.craftOrder(2)
+        }
+
+        craftGlassTableButton.setOnClickListener {
+            game.craftOrder(3)
+        }
+
+        craftParkBenchButton.setOnClickListener {
+            game.craftOrder(4)
+        }
+
+        craftPlasticChairButton.setOnClickListener {
+            game.craftOrder(5)
+        }
+
+        craftStoneBenchButton.setOnClickListener {
+            game.craftOrder(6)
+        }
+
+        craftStoneTable.setOnClickListener {
+            game.craftOrder(7)
         }
 
         gameOverButton.setOnClickListener {
